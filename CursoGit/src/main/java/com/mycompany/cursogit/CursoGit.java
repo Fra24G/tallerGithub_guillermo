f0b59e1cf30d3e0529faf8bdaa9f2767fa7db21e
Nafitafi <nafibriones@gmail.com>
2026-02-12 17:34:48 -0600

    frix hicechamba q no hiciste
    Lol q mal

--- CursoGit/src/main/java/com/mycompany/cursogit/CursoGit.java
+++ CursoGit/src/main/java/com/mycompany/cursogit/CursoGit.java
@@ -12,9 +12,7 @@ public class CursoGit {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-<<<<<<< Updated upstream
-        System.out.println("Hecho en itson");
-=======
->>>>>>> Stashed changes
+        System.out.println("Hecho por la mera eminencia");
+        System.out.println("Hermano realmente no escribió nada más en su print");
     }
 }
